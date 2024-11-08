27ce5b07cadd62561c9fa17a6890be301570a919
Daniel Wei <weidaniel15@gmail.com>
2024-11-08 01:58:22 -0500

    Rewrite Everything

--- src/pages/blog/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-type RootLayoutProps = { children: ReactNode };
-
-export default async function RootLayout({ children }: RootLayoutProps) {
-  return <div className="w-full lg:ml-12 prose">{children}</div>;
-}
