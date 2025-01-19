c07da6b4d3cc5fc7fceef9e004024d96c1808a4c
Daniel Wei <weidaniel15@gmail.com>
2025-01-19 11:27:11 -0500

    Add Umami Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Cover } from "@/components/cover";
 import { Navbar } from "@/components/navbar";
 import { FC, PropsWithChildren } from "react";
 import "./globals.css";
+import Script from "next/script";
 
 const Layout: FC<PropsWithChildren> = (props) => {
   return (
@@ -13,8 +14,13 @@ const Layout: FC<PropsWithChildren> = (props) => {
         <meta name="author" content="Daniel Wei" />
         <meta name="keywords" content="Daniel Wei, w3i, personal website" />
         <meta name="robots" content="index, follow" />
-        <script src="https://cdn.tailwindcss.com" />
-        <script src="https://unpkg.com/htmx.org@2.0.3" />
+        <Script src="https://cdn.tailwindcss.com" />
+        <Script src="https://unpkg.com/htmx.org@2.0.3" />
+        <Script
+          defer
+          src="https://umami.wednesdays.me/script.js"
+          data-website-id="d54a19a0-8d7c-4f7f-9a95-d29fa93a014b"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
